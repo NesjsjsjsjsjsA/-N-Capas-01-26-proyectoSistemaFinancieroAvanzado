503a79fdb6be414e59241d84e73ab33aaeec8f04
Néstor Ayala <127457394+NesjsjsjsjsjsA@users.noreply.github.com>
2026-06-29 00:07:41 -0600

    Agregando más entidades

--- SistemaAvanzadoFinanciero/src/main/java/com/server/app/entities/Movimiento.java
+++ SistemaAvanzadoFinanciero/src/main/java/com/server/app/entities/Movimiento.java
@@ -1,4 +1,44 @@
 package com.server.app.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Movimiento_data")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Movimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String moneda_Original;
+
+    private Integer Tasa_Cambio;
+
+    private Integer monto;
+
+    private Date fecha;
+
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "cuenta_id", nullable = false, foreignKey = @ForeignKey(name ="fk_cuenta_id"))
+    private Cuenta cuenta;
+
+    @ManyToOne
+    @JoinColumn(name="categoria_id", nullable = false, foreignKey = @ForeignKey(name = "fk_categoria_id"))
+    private Categoria categoria;
+
+
 }
